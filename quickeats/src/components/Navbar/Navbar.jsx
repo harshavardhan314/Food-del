36127a715e242ac28a79e5eaa7c68209c3b4f938
harshavardhan314 <hharshavardhan974@gmail.com>
2025-09-03 18:58:35 +0530

    navbar

---
+++ quickeats/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt="logo" className='logo'/>
+        <ul className='nav-items'>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Contact us</li>
+            <li>Mobile app</li>
+        </ul>
+        <div className="nav-right">
+            <img src={assets.search_icon} alt="search" className='nav-icon'/>
+            <div className="cart-icon">
+                <img src={assets.basket_icon} alt="cart" className='nav-icon'/>
+            </div>
+             <button className='sign-in'>sign in</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
