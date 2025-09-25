a6b1f81ff105a33ad8c75048a4586886a4c03e59
harshavardhan314 <hharshavardhan974@gmail.com>
2025-09-25 16:00:48 +0530

    developed home page component

---
+++ quickeats/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+import { assets } from '../../assets/assets'
+const Header = () => {
+  return (
+    <div>
+        <div className="Header">
+
+            <div className="Header-content">
+                    <h1>Good food choices are good investments.</h1>
+                    <p>Start your day with exclusive breakfast, lunch and dinner options from your favourite restaurants.</p>
+                    <button className='explore-menu'>Explore menu</button>
+
+            </div>
+           
+
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- quickeats/src/pages/Home/Home.jsx
+++ quickeats/src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Header></Header>
+    </div>
   )
 }
 
