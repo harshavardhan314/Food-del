e44d3d63dcb721cd649deb21681be26656104d50
harshavardhan314 <hharshavardhan974@gmail.com>
2025-09-26 16:34:56 +0530

    created menu page

--- quickeats/src/components/Header/Header.jsx
+++ quickeats/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import { assets } from '../../assets/assets'
+import {assets,food_list,menu_list} from '../../assets/assets'
 const Header = () => {
   return (
     <div>
@@ -10,14 +10,45 @@ const Header = () => {
                     <h2>Order Your Favorite Food here.</h2>
                     <p>Choose from a diverse menu of restaurants and cuisines, delivered straight to your doorstep
                     our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-                    <button className='explore-menu'>Explore menu</button>
-
+                    <button className='explore-menu'>View menu</button>
             </div>
-           
-
-
-
         </div>
+        <div className="menu">
+              <h3>Explore Our Menu</h3>
+              <p>Choose from a diverse menu of restaurants and cuisines, delivered straight to your doorstep
+                    our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        </div>
+        <div className="menu-items">
+
+          {
+          menu_list.map((item, idx) => (
+
+            <div key={idx} className="menu-item">
+              <img src={item.menu_image} alt={item.menu_name} />
+              <p>{item.menu_name}</p>
+            </div>
+
+        ))}
+        </div>
+
+          <div className="Food-list">
+
+          <h2>Top dishes near You</h2>
+          <p>Discover the most popular dishes in your area, handpicked by our food experts to satisfy your cravings and delight your taste buds.</p>
+
+          <div className="food-list-grid">
+      {food_list.map((item, idx) => (
+        <div key={idx} className="food-item">
+          <img src={item.image} alt={item.name} />
+          <p>{item.name}</p>
+          <p>{item.description}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
+      </div>
+          
+        </div>
+
     </div>
   )
 }
