8fd32640c28dcea38353163b7f1eb22ff7e84b6b
harshavardhan314 <hharshavardhan974@gmail.com>
2025-10-21 19:17:57 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoutes = require("./Routes/userroutes");
 
-const express=require('express');
-const app=express();
-const port=5000;
+dotenv.config();
+const app = express();
+const PORT = 5000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello ');
-});
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use("/users", userroutes);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
